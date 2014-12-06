552bb5ac31e9d6684b4bd48195466b369e83b22b
Rus Fillmore <rx.fillmore@gmail.com>
2014-12-05 22:11:43 -0500

    *^* c35's showing up

--- src/main/resources/static/js/callcenter.js
+++ src/main/resources/static/js/callcenter.js
@@ -3,11 +3,21 @@ var app = angular.module('CCApp', []);
 app.controller("MainController", function($scope, $http, $interval){
 	$scope.view = "home";
 	$scope.requestList =[];
+	tempBB =[];
+	$scope.bathroomRequest =[];
+	
 
 	$scope.getList = function(){
 		$http.get("/getList").success(function(data){
 			$scope.requestList = data;
-			//console.log("do");
+			tempBB=[];
+			for(i=0;i<$scope.requestList.length; i++){
+				if($scope.requestList[i].bathroomBreak){
+					tempBB.push($scope.requestList.splice(i,1)[0]);
+				}
+			}
+			console.log(tempBB);
+			$scope.bathroomRequest = tempBB;
 		}).error(function(){
 			alert("List Failed")
 		})
