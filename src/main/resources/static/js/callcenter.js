05e405cedc37047bd3bd7dd7483408ed955a33cd
Rus Fillmore <rx.fillmore@gmail.com>
2014-12-11 21:27:16 -0500

    *^* advisory level is working woot at least the start is

--- src/main/resources/static/js/callcenter.js
+++ src/main/resources/static/js/callcenter.js
@@ -5,6 +5,7 @@ app.controller("MainController", function($scope, $http, $interval){
 	$scope.requestList =[];
 	tempBB =[];
 	$scope.bathroomRequest =[];
+	$scope.advisoryLevel = null;
 	
 
 	$scope.getList = function(){
@@ -23,6 +24,16 @@ app.controller("MainController", function($scope, $http, $interval){
 		})
 	}
 	
+	$scope.getAdvisoryLevel = function(){
+		$http.get("/getAdvisory").success(function(data){
+			advisoryLevel=data;
+		}).error(function(){
+			alert("Advisory Failed");
+		})
+		console.log(advisoryLevel);
+	}
+	
 	$interval($scope.getList, 5000);
+	$interval($scope.getAdvisoryLevel, 5000);
 	
 });
