a5e456e9beb4133948bc98afd9d5e6d01998b055
Rus Fillmore <rx.fillmore@gmail.com>
2014-12-11 22:48:46 -0500

    *^* working on admin

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,4 +1,12 @@
 app.controller("AdminController", function($scope, $http, $interval) {
 	
+	$scope.changeGroups = function(groups){
+		$http.get("/changeGroup",{
+			params: { group: groups}
+		}).success(function(data){
+		}).error(function(){
+			alert("Group Change Failed");
+		})
+	}
 	
 }
