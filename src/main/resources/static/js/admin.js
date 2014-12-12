51ca40ce74d4c3e6d304a6d88b367ac1ce15484a
Rus Fillmore <rx.fillmore@gmail.com>
2014-12-11 23:20:11 -0500

    *^* who knows

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -4,6 +4,17 @@ app.controller("AdminController", function($scope, $http, $interval) {
 		$http.get("/changeGroup",{
 			params: { group: groups}
 		}).success(function(data){
+			$scope.getAdvisoryLevel();
+		}).error(function(){
+			alert("Group Change Failed");
+		})
+	}
+	
+	$scope.changeGroups = function(num){
+		$http.get("/setCutOff",{
+			params: { number: num}
+		}).success(function(data){
+			$scope.getAdvisoryLevel();
 		}).error(function(){
 			alert("Group Change Failed");
 		})
